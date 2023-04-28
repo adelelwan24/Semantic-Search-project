ce429b27a9be8b8e61d3c5ee77fa15e016eab274
adelelwan24 <adelelwan24@gmail.com>
2023-04-28 22:30:49 +0200

    Handle imports and order the routes

--- Server/routes.py
+++ Server/routes.py
@@ -1,9 +1,20 @@
-from Server import app
-from Server.utils import Pre_Process, Search_VDB, Format_Results
-from flask import request, Response, abort, jsonify
+from Server import app, db, bcrypt
+from Server.Utils.utils import Exception_Info
+from Server.Utils.pre_processor import Pre_Process, Format_Results
+from Server.Utils.vdb import Search_VDB
+from flask import request, abort, jsonify
 from flask_api import status ,exceptions
+from .schemas import UserCreationSchema, UserLoginSchema, UserSchema, ValidationError
+from sqlalchemy.exc import SQLAlchemyError
+from .models import User
 
 
+userSchema = UserSchema()
+userCreationSchema = UserCreationSchema()
+userLoginSchema = UserLoginSchema()
+
+
+    
 
 @app.route('/')
 @app.route('/home')
@@ -11,12 +22,79 @@ def home():
     return 'hello world'
 
 
+################################################################################
+'''
+Users
+'''
+################################################################################
+
+@app.route('/users')
+@app.route('/users/all')
+def all_users():
+    users = User.query.all()
+    return jsonify(userSchema.dump(users, many=True))
+
+@app.route('/users/<int:id>')
+def get_user_by_id(id):
+    user = User.query.filter(User.id == id ).first_or_404()
+    return jsonify(userSchema.dump(user))
+
+@app.route('/users/create', methods=['post'])
+def create_user():
+
+    data = userCreationSchema.load(data=request.json)
+    user = User(**data)   
+
+    try:
+        db.session.add(user)
+        db.session.commit()
+        db.reflect(user)
+    except TypeError as e:
+        Exception_Info()
+    except SQLAlchemyError as exe:
+        db.session.rollback()
+        Exception_Info()
+        raise exe
+
+    return jsonify(data), status.HTTP_201_CREATED
+
+@app.route('/users/login', methods=['post'])
+def login_user():
+    data = userLoginSchema.load(request.json)
+    user = User.query.filter(User.email_address == data['email_address']).one_or_none()
+    if not user:
+        abort(status.HTTP_404_NOT_FOUND, "Email Doesn't Exist")
+    if user and bcrypt.check_password_hash(user.password_hash, data['password']):
+        return jsonify({'messege' : 'Successfully Logged In'})
+    abort(status.HTTP_401_UNAUTHORIZED)
+    abort(status.HTTP_401_UNAUTHORIZED, "Password Is Invalid")
+    
+
+@app.route('/users/<int:id>', methods= ['delete'])
+def delete_user(id):
+    user = User.query.filter(User.id == id).one_or_none()
+    if not user:
+        abort(status.HTTP_404_NOT_FOUND, "User Doesn't Exist")
+
+    db.session.delete(user)
+    db.session.commit()
+    db.reflect(user)
+    return jsonify(userSchema.dump(user)), status.HTTP_204_NO_CONTENT
+
+
+################################################################################
+'''
+Search
+'''
+################################################################################
+
+
 @app.route('/apis/videos/search', methods=['post'])
 def search_video():
     query = request.form['query']
     print(query)
-    # query = Pre_Process(query)
-    # results = Search_VDB(query)
+    processed_query = Pre_Process(query)
+    results = Search_VDB(processed_query)
     return query
 
 
@@ -28,32 +106,7 @@ def search_papers():
     else:
         query = request.form['query']
     print(query)
-    # query = Pre_Process(query)
-    # results = Search_VDB(query)
+    processed_query = Pre_Process(query)
+    results = Search_VDB(processed_query)
     return query
 
-
-################################################################################
-'''
-Error Handeler
-'''
-################################################################################
-
-@app.errorhandler(status.HTTP_404_NOT_FOUND)
-def error_handler_404(error):
-    return jsonify({
-        'code' : status.HTTP_404_NOT_FOUND,
-        'message' : 'Page not found'
-        }), status.HTTP_404_NOT_FOUND
-
-@app.errorhandler(422)
-def error_handler_422(error):
-    return jsonify({'code' : 422,
-        'message' : 'Unprocessable Entity',
-        }), 422
-
-@app.errorhandler(status.HTTP_500_INTERNAL_SERVER_ERROR)
-def error_handler_500(error):
-    return jsonify({'code' : status.HTTP_500_INTERNAL_SERVER_ERROR,
-        'message' : 'Server Side Error',
-        }), status.HTTP_500_INTERNAL_SERVER_ERROR
