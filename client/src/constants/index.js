7c91d78ec7fcd5bb4e521934a20f601e50a2c964
behairy <github.com/Behairy99>
2023-05-02 16:47:23 +0300

    Add client

---
+++ client/src/constants/index.js
@@ -0,0 +1,35 @@
+// the content in our app
+
+export const navLinks = [
+    
+    
+    {
+      id: "searchType",
+      title: "SearchType",
+    },
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+
+
+  const services = [
+    {
+      title: 'Videos',
+      description: "Find in your videos much easier",
+      image: '/images/video.jpg',
+      id: 0,
+    },
+    {
+      title: 'Articles',
+      description:"Find in your Articles much easier.",
+      image: '/images/document.jpg',
+      id: 1,
+    },
+    
+  ];
