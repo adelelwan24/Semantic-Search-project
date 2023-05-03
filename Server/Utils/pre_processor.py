16601285251c1fa83117990ed587f44d5622239c
adelelwan24 <adelelwan24@gmail.com>
2023-05-03 23:30:24 +0200

    Implement the Format results function

--- Server/Utils/pre_processor.py
+++ Server/Utils/pre_processor.py
@@ -1,10 +1,20 @@
-
+from pymilvus.orm.search import  SearchResult
+from typing import List
 
 
 def Pre_Process(query: str) -> str:
-    pass
+    return query
 
 
 
-def Format_Results(results):
-    pass
+def Format_Results(results : SearchResult) -> List[dict]:
+    results = results[0]
+    ret = []
+
+    for match in results:
+        data = {}
+        data['id'] = match.id
+        for key in match.entity.fields:
+            data[key] = match.entity.get(key)
+        ret.append(data)
+    return ret
