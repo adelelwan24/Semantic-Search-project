caf8704744125ded3f526e4be82b8c771f2e57c3
adelelwan24 <adelelwan24@gmail.com>
2023-05-04 10:17:09 +0200

    Add Configuratiion file

---
+++ Server/config.py
@@ -0,0 +1,8 @@
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
