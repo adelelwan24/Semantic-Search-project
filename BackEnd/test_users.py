a4413f18476a52048605993595ebd5167deaf0e8
adelelwan24 <adelelwan24@gmail.com>
2023-06-26 04:01:13 +0200

    Update tests with authentication and authorization

--- BackEnd/test_users.py
+++ BackEnd/test_users.py
@@ -35,15 +35,27 @@ class UserTest(unittest.TestCase):
             # create all tables
             self.db.create_all()
 
+        #### TODO: Make the user admin or test with admin and no admin
+        #### Create the user
         payload = {
-            'username' : 'test_username24',
-            'email_address' : 'test24@email.com',
+            'username' : 'test_username',
+            'email_address' : 'test@email.com',
             'password' : "Test1234"
                 }
         
         res = self.client().post('/users/create', json=payload) 
         self.assertEqual(res.status_code, 201)
 
+        #### Login the user
+        payload_login = {
+            "email_address" : "test@email.com",
+            "password": "Test1234"
+            }
+        res_login = self.client().post('/users/login', json=payload_login) 
+        self.assertEqual(res_login.status_code, 200)
+        self.jwt = res_login.json['token']
+
+        self.headers = {'Authorization' : f'bearer {self.jwt}'}
 
 
     def tearDown(self):
@@ -57,14 +69,7 @@ class UserTest(unittest.TestCase):
     """
 
     def test_get_users(self):
-        res = self.client().get('/users/')
-        data = res.json
-
-        self.assertEqual(res.status_code , 200)
-        self.assertIsInstance(data, list)
-
-    def test_get_all_users(self):
-        res = self.client().get('/users/')
+        res = self.client().get('/users/', headers = self.headers)
         data = res.json
 
         self.assertEqual(res.status_code , 200)
@@ -72,7 +77,7 @@ class UserTest(unittest.TestCase):
 
 
     def test_get_one_user(self):
-        res = self.client().get('/users/1')
+        res = self.client().get('/users/1', headers = self.headers)
         data = res.json
 
         self.assertEqual(res.status_code , 200)
@@ -85,12 +90,12 @@ class UserTest(unittest.TestCase):
     
     def test_create_user(self):
         payload = {
-            'username' : 'test_username',
-            'email_address' : 'test@email.com',
+            'username' : 'test_username_new',
+            'email_address' : 'test_new@email.com',
             'password' : "Test1234"
                 }
         
-        res = self.client().post('/users/create', json=payload) 
+        res = self.client().post('/users/create', json=payload, headers = self.headers) 
         data = res.json
 
         self.assertEqual(res.status_code , 201)
@@ -103,8 +108,8 @@ class UserTest(unittest.TestCase):
 
     def test_create_duplicate_user(self):
         payload = {
-            'username' : 'test_username24',
-            'email_address' : 'test24@email.com',
+            'username' : 'test_username',
+            'email_address' : 'test@email.com',
             'password' : "Test1234"
                 }
         
@@ -190,7 +195,7 @@ class UserTest(unittest.TestCase):
 
     def test_login_user(self):
         payload = {
-            'email_address' : 'test24@email.com',
+            'email_address' : 'test@email.com',
             'password' : "Test1234"
                 }
         
@@ -204,7 +209,7 @@ class UserTest(unittest.TestCase):
 
     def test_login_user_wrong_pass(self):
         payload = {
-            'email_address' : 'test24@email.com',
+            'email_address' : 'test@email.com',
             'password' : "Test43221"
                 }
         
@@ -221,12 +226,12 @@ class UserTest(unittest.TestCase):
         self.assertEqual(res.status_code , 404)
 
     def test_delete_user(self):
-        res = self.client().delete('/users/1') 
+        res = self.client().delete('/users/1',  headers = self.headers) 
         self.assertEqual(res.status_code , 204)
 
-    def test_delete_user_404(self):
-        res = self.client().delete('/users/1000') 
-        self.assertEqual(res.status_code , 404)
+    def test_delete_user_does_not_exist(self):
+        res = self.client().delete('/users/1000', headers = self.headers) 
+        self.assertEqual(res.status_code , 403)
     
 # Make the tests conveniently executable
 if __name__ == "__main__":
