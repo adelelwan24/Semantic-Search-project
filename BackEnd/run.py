1f934b5718f904be4f4fd68f1adf00b1ea30f4e1
adelelwan24 <adelelwan24@gmail.com>
2023-07-03 06:01:42 +0200

    Re-Factor

--- BackEnd/run.py
+++ BackEnd/run.py
@@ -1,6 +1,18 @@
-from Server import create_app
+from Server import create_app, db
 
 app = create_app()
 
+# try:
+#     with app.app_context():
+#         db.create_all()
+# except:
+#     print(f'FAiLED TO CREATE TABLES IN DB')
+
+
+#### For Development Only
+@app.route('/')
+def hello():
+    return '<h1>Welcome To Search Mate</h1>'
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5030)
+    app.run(debug=True, host= '0.0.0.0', port=5000, )
