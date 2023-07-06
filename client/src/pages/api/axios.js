a9f25fa446baf790a3af617ba58bb21ead1d7166
adelelwan24 <adelelwan24@gmail.com>
2023-07-07 00:56:01 +0200

    Write the base URL for axios

--- client/src/pages/api/axios.js
+++ client/src/pages/api/axios.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
     // put the development backend will run at which port 
-    baseURL: 'http://localhost:3005'
+    baseURL: 'http://localhost:5000'
 })
