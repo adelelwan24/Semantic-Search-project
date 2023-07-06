7c12fc2699f890bc25e24f84599e5a2f3917f581
adelelwan24 <adelelwan24@gmail.com>
2023-07-07 01:07:30 +0200

    Polish Cache configurations

--- BackEnd/Server/config.py
+++ BackEnd/Server/config.py
@@ -21,5 +21,5 @@ class Config:
     CACHE_REDIS_HOST = os.environ['CACHE_REDIS_HOST']
     CACHE_REDIS_PORT = os.environ['CACHE_REDIS_PORT']
     CACHE_REDIS_DB = os.environ['CACHE_REDIS_DB']
-    CACHE_REDIS_URL = os.environ['CACHE_REDIS_URL']
+    # CACHE_REDIS_URL = os.environ['CACHE_REDIS_URL']
     CACHE_DEFAULT_TIMEOUT = os.environ['CACHE_DEFAULT_TIMEOUT']
